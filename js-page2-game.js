c77c80881ba0a34748edd978d829867eb004d6de
andreagromico <andreagromico@gmail.com>
2021-10-25 21:01:23 +0700

    Commit Chapter 4

--- js-page2-game.js
+++ js-page2-game.js
@@ -1,111 +1,158 @@
 // Define Variable
 
-var player = ""
-var com_choose = ""
-var bg_choose_com = ""
-var bg_choose_player = ""
+// player
+const player_batu = document.getElementById("player-batu")
+const player_kertas = document.getElementById("player-kertas")
+const player_gunting = document.getElementById("player-gunting")
+
+// computer
+const com_batu = document.getElementById("com-batu")
+const com_kertas = document.getElementById("com-kertas")
+const com_gunting = document.getElementById("com-gunting")
+
+// result & reload
+const show_result = document.getElementById("output-result");
+const reload_click = document.getElementById("reload-player-click");
 
 
-// Logic for Player's Choice
+//Parent Class Game & method
+class Game{
+  constructor(computerShuffle, computerChoice, game_output, result_showing) {
+      this.computerShuffle = computerShuffle
+      this.computerChoice = computerChoice
+      this.game_output = game_output
+      this.result_showing = result_showing
+  }
 
-function choose(element){
-    player = (element.getAttribute("value"));
-    com = Math.floor(Math.random() * 101);
+  //Choose logic
+  choose(){
+      const computerShuffle = ["ROCK", "PAPER", "SCISSORS"];
+      this.computerChoice = computerShuffle[Math.floor(Math.random() * computerShuffle.length)];
+  }
 
-    if (player == "ROCK"){
-        bg_choose_player = document.getElementById("player-batu");
-        bg_choose_player.style.backgroundColor = "silver";
-        document.getElementById("player-batu").style.pointerEvents = 'none';
-        document.getElementById("player-kertas").style.pointerEvents = 'none';
-        document.getElementById("player-gunting").style.pointerEvents = 'none';
-        
-        
+  //Logic game
+  logicalmethod(){
+      if(this.playerChoice == this.computerChoice){
+          this.game_output = "Draw"
+          this.result_showing = "DRAW"
+          this.gameResult()
+          
+      }else if (
+          (this.playerChoice === "ROCK" && this.computerChoice === "SCISSORS") 
+          || (this.playerChoice === "SCISSORS" && this.computerChoice === "PAPER") 
+          || (this.playerChoice === "PAPER" && this.computerChoice === "ROCK")
+          ){
+              this.game_output = "PlayerWin"
+              this.result_showing = "PLAYER 1 WIN"
+              this.gameResult()
 
-    }else if(player == "SCISSORS"){
-        bg_choose_player = document.getElementById("player-gunting");
-        bg_choose_player.style.backgroundColor = "silver";
-        document.getElementById("player-batu").style.pointerEvents = 'none';
-        document.getElementById("player-kertas").style.pointerEvents = 'none';
-        document.getElementById("player-gunting").style.pointerEvents = 'none';
-        
-        
+      }else{
+          this.game_output = "ComputerWin"
+          this.result_showing = "COM WIN"
+          this.gameResult()
+      }
+  }
 
-    }else {
-        bg_choose_player = document.getElementById("player-kertas");
-        bg_choose_player.style.backgroundColor = "silver";
-        document.getElementById("player-batu").style.pointerEvents = 'none';
-        document.getElementById("player-kertas").style.pointerEvents = 'none';
-        document.getElementById("player-gunting").style.pointerEvents = 'none';
-        
-        
+  //output
+  gameResult(){
+      show_result.classList.remove("middle-versus")
+      show_result.classList.add(this.game_output)
+      show_result.innerHTML = this.result_showing;  
+      console.log(this.result_showing)
+  }
+
+  // stop option supaya setelah player click pilihan, kemudian tidak bisa click pilihan lain 
+  stop_option_freeze(){
+      player_batu.style.pointerEvents = "none"
+      player_kertas.style.pointerEvents = "none"
+      player_gunting.style.pointerEvents = "none"
+  }
+
+  // game reload
+  refresh(){
+      reload_click.addEventListener("click", () => {
+      player_batu.style.pointerEvents = 'auto';
+      player_kertas.style.pointerEvents = 'auto';
+      player_gunting.style.pointerEvents = 'auto';
+
+      player_batu.style.backgroundColor = "#9C835F";
+      player_kertas.style.backgroundColor = "#9C835F";
+      player_gunting.style.backgroundColor = "#9C835F";
+      com_batu.style.backgroundColor = "#9C835F";
+      com_kertas.style.backgroundColor = "#9C835F";
+      com_gunting.style.backgroundColor = "#9C835F";
+
+
+      show_result.classList.remove("Draw");
+      show_result.classList.remove("PlayerWin");
+      show_result.classList.remove("ComputerWin");
+      show_result.classList.add("middle-versus");
+      show_result.innerHTML = "VS";
+      
+      this.playerChoice=""
+      this.shuffle=""
+
+      })
+  }
+
+}
+
+// Inheritance 
+class PlayerCom extends Game{
+  constructor(computerShuffle,computerChoice,playerChoice) {
+      super(computerShuffle,computerChoice)
+      this.playerChoice = playerChoice  
+  }
+  
+  ROCK(){
+      player_batu.addEventListener("click", () => {
+      this.playerChoice = "ROCK";
+      player_batu.style.backgroundColor = "silver"
+      super.choose()
+      this._comBg()
+      this.logicalmethod()
+      this.stop_option_freeze()
+      })
+  }
+
+  PAPER(){
+    player_kertas.addEventListener("click", () => {
+    this.playerChoice = "PAPER"
+    player_kertas.style.backgroundColor = "silver"
+    super.choose()
+    this._comBg()
+    this.logicalmethod()
+    this.stop_option_freeze()
+    })
+}
+
+  SCISSORS(){
+      player_gunting.addEventListener("click", () => {
+      this.playerChoice = "SCISSORS";
+      player_gunting.style.backgroundColor = "silver"
+      super.choose()
+      this._comBg()
+      this.logicalmethod()
+      this.stop_option_freeze()
+      })
+  }
+
+  _comBg(){
+    if(this.computerChoice == "ROCK"){
+        com_batu.style.backgroundColor = "silver";
+    }else if (this.computerChoice == "PAPER"){
+        com_kertas.style.backgroundColor = "silver";
+    }else{
+        com_gunting.style.backgroundColor = "silver";
     }
+}
+}
 
 
-    //Computer's Choice in respon to player's choice
-
-    if (com <= 33 && com >0) {
-        console.log("Computer choose ROCK " + "value " + com);
-        com_choose = "ROCK"
-        bg_choose_com = document.getElementById("com-batu");
-        bg_choose_com.style.backgroundColor = "silver";
-        
-    } else if (com >33 && com<=66){
-        console.log("Computer choose SCISSORS " + "value " + com);
-        com_choose = "SCISSORS"
-        bg_choose_com = document.getElementById("com-gunting");
-        bg_choose_com.style.backgroundColor = "silver";
-       
-    } else {
-        console.log("Computer choose PAPER " + "value " + com);
-        com_choose = "PAPER"
-        bg_choose_com = document.getElementById("com-kertas");
-        bg_choose_com.style.backgroundColor = "silver";        
-    }
-
-    
-
-    //Game Logic and Selection
-
-    if (player == com_choose){
-        document.getElementById("output-result").classList.remove('middle-versus');
-        document.getElementById("output-result").classList.add('DRAW');
-        
-    } else if (
-        (player == "ROCK" && com_choose == "SCISSORS")  
-        || (player == "PAPER" && com_choose == "ROCK")
-        || (player == "SCISSORS" && com_choose == "PAPER")
-        ){
-        document.getElementById("output-result").classList.remove('middle-versus');
-        document.getElementById("output-result").classList.add('PlayerWin');
-        
-    } else {
-        document.getElementById("output-result").classList.remove('middle-versus');
-        document.getElementById("output-result").classList.add('ComputerWin');
-       
-    }
-} 
-
-
-// Logic for Reload and Refresh Game
-
-function reload(element){
-        document.getElementById("player-gunting").style.pointerEvents = 'auto';
-        document.getElementById("player-kertas").style.pointerEvents = 'auto';
-        document.getElementById("player-batu").style.pointerEvents = 'auto';
-
-        document.getElementById("player-batu").style.backgroundColor = "#9C835F";
-        document.getElementById("player-gunting").style.backgroundColor = "#9C835F";
-        document.getElementById("player-kertas").style.backgroundColor = "#9C835F";
-        
-        document.getElementById("com-batu").style.backgroundColor = "#9C835F";
-        document.getElementById("com-gunting").style.backgroundColor = "#9C835F";
-        document.getElementById("com-kertas").style.backgroundColor = "#9C835F";
-
-        document.getElementById("output-result").classList.remove('DRAW');
-        document.getElementById("output-result").classList.remove('PlayerWin');
-        document.getElementById("output-result").classList.remove('ComputerWin');
-
-        document.getElementById("output-result").classList.add('middle-versus');
-        
-   
-    }
+const play = new PlayerCom();
+play.ROCK()
+play.PAPER()
+play.SCISSORS()
+  
+const repeat = new Game()
+repeat.refresh()  
